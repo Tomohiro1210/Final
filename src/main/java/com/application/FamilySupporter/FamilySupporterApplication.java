20d3fb2125d303cf00d22b0a8269dca62e65e49e
Tomohiro1210 <dzksmf-etc@yahoo.co.jp>
2023-07-26 23:57:02 +0900

    実装前のコミット

---
+++ src/main/java/com/application/FamilySupporter/FamilySupporterApplication.java
@@ -0,0 +1,13 @@
+package com.application.FamilySupporter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FamilySupporterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FamilySupporterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/application/FamilySupporter/FamilySupporterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.application.FamilySupporter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FamilySupporterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
